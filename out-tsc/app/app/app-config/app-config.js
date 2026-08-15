4f07f92e172024a4f96f6a5b1a485c76a906595e
Daniel Corredor <dann95.moz@gmail.com>
2026-08-15 18:15:53 -0500

    chore: add out-tsc to .gitignore and remove compiler artifacts from git tracking

--- out-tsc/app/app/app-config/app-config.js
+++
@@ -1,3 +0,0 @@
-export const APP_CONFIG = {
-    version: 'profesores',
-};
--- out-tsc/app/app/app.routes.js
+++
@@ -1,7 +0,0 @@
-import { EditorPageComponent } from './features/editor/editor-page/editor-page.component';
-export const routes = [
-    {
-        path: '',
-        component: EditorPageComponent,
-    },
-];
